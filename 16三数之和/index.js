247be4016b2b4b10fe605a2fd4dd821b962c7735
zuogerenba <1732935973@qq.com>
2020-05-20 17:07:39 +0800

    三数之和解题思路

--- 16三数之和/index.js
+++ 16三数之和/index.js
@@ -1,3 +1,31 @@
-var threeSumClosest = function(nums, target) {
-  
+var threeSumClosest = function (nums) {
+  let result = []
+  nums.sort(function (a, b) {
+    return a - b
+  })
+  // console.log(nums)
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i === 0 || nums[i] !== nums[i - 1]) {
+      let start = i + 1, end = nums.length - 1
+      while (start < end) {
+        if (nums[i] + nums[start] + nums[end] === 0) {
+          result.push([nums[i], nums[start], nums[end]])
+          start++
+          end--
+          while (start < end && nums[start] === nums[start - 1]) {
+            start++
+          }
+          while (start < end && nums[end] === nums[end + 1]) {
+            end--
+          }
+        } else if (nums[i] + nums[start] + nums[end] < 0) {
+          start++
+        } else {
+          end--
+        }
+      }
+    }
+  }
+  return result
 };
+console.log(threeSumClosest([-1, 0, 1, 2, -1, -4]))
