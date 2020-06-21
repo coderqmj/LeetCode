c5ba692e106f675f700b2e9fb6d4cbbb62c52469
zuogerenba <1732935973@qq.com>
2020-06-21 19:33:21 +0800

    刷题

--- 26数组去重/index.js
+++ 26数组去重/index.js
@@ -1,6 +1,17 @@
-var removeDuplicates = function(nums) {
-  return Array.from(new Set(nums)).length
+// var removeDuplicates = function(nums) {
+//   return Array.from(new Set(nums)).length
+// };
+// var nums = [1,1,2]
+// console.log(removeDuplicates(nums));
+// console.log(nums);
+
+var removeDuplicates = function (nums) {
+  let j = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[j]) {
+      j++;
+      nums[j] = nums[i];
+    }
+  }
+  return j + 1
 };
-var nums = [1,1,2]
-console.log(removeDuplicates(nums));
-console.log(nums);
