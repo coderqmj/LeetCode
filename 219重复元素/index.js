73316c02fb528e3e54ef06153787c5c38bb68fdd
mj.qiu <mj.qiu@tuya.com>
2020-10-12 19:52:35 +0800

    每日一題

---
+++ 219重复元素/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  if(k<0) return false;
+  let map = new Map();
+  for(let i =0;i<nums.length;i++) {
+    if(map.has(nums[i])) {
+      if(i-map.get(nums[i]) <= k ) {
+        return true
+      } else { 
+        map.set(nums[i],i)
+      }
+    }else {
+      map.set(nums[i],i)
+    }
+  }
+  return false
+};
+
+console.log(containsNearbyDuplicate([1,2,3,1], 3))
