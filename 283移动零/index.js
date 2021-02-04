47facbf02a8419ab8c45b4c02a6b9c889f3c83ac
zuogerenba <1732935973@qq.com>
2021-02-04 19:47:42 +0800

    2021/02/04刷题

---
+++ 283移动零/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let count = 0;
+  for(let i = 0;i<nums.length;i++) {
+      if(nums[i]===0) count++;
+  }
+  for(let i =0;i<nums.length;i++) {
+      if(nums[i] === 0) {
+          nums.splice(i, 1);
+          i--;
+      }
+  }
+  for(let i =0;i<count;i++) {
+      nums.push(0);
+  }
+  console.log(nums, count)
+};
+console.log(moveZeroes([0,0]))
