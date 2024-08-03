b0d0d0af7b9af99e4950950c79b8564478fcc57a
coderqmj <1732935973@qq.com>
2024-08-03 15:00:39 +0800

    feat: 更新

---
+++ 215数组中第k大的数/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  let arr = [...nums];
+  let maxArr = []
+  
+  return maxArr.at(-1)
+};
+
+
+
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
