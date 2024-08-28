ae941a8a92d05eb2604fa17a592543cccb379ba1
coderqmj <1732935973@qq.com>
2024-08-28 09:26:52 +0800

    update blog

--- 53最大子序和/index.js
+++ 53最大子序和/index.js
@@ -1,11 +1,13 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * 动态规划
+ * 思路：1.动态规划，将所有元素的当前利益算出来，和最大值比较
+ *
  */
+
 var maxSubArray = function (nums) {
   let dp = [];
+  let max = 0;
   dp[0] = nums[0];
-  let max = dp[0];
   for (let i = 1; i < nums.length; i++) {
     if (dp[i - 1] < 0) {
       dp[i] = nums[i];
